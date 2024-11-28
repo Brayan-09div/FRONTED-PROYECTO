0afe50b054ffa81b824428fcc1b967d7d7be7910
Tatiana <tatianamantilla547@gmail.com>
2024-11-28 15:51:11 -0500

    cambio en la ruta en axios

--- src/plugins/Axios.js
+++ src/plugins/Axios.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 const token = JSON.parse(localStorage.getItem('auth')); 
  
 const apiClient = axios.create({
-  baseURL: 'http://localhost:4500/api',
-  // baseURL: 'https://etapas-practicas-ep.onrender.com/api',
+  // baseURL: 'http://localhost:4500/api',
+  baseURL: 'https://etapas-practicas-ep.onrender.com/api',
   headers: {
     "token": token.token
   }
