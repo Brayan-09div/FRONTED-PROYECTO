9398b079dcda805cd480a838114521a6b8672b70
Tatiana <tatianamantilla547@gmail.com>
2024-11-28 19:04:40 -0500

    cambios de rutas

--- src/plugins/Axios.js
+++ src/plugins/Axios.js
@@ -1,15 +1,39 @@
+// import axios from 'axios';
+
+
+// const token = JSON.parse(localStorage.getItem('auth')); 
+ 
+// const apiClient = axios.create({
+//   // baseURL: 'http://localhost:4500/api',
+//   baseURL: 'https://etapas-practicas-ep.onrender.com',
+//   headers: {
+//     "token": token.token
+//   }
+// });
+
+
+// export default apiClient;
+
 import axios from 'axios';
 
+const token = localStorage.getItem('token') || '';
 
-const token = JSON.parse(localStorage.getItem('auth')); 
- 
 const apiClient = axios.create({
-  // baseURL: 'http://localhost:4500/api',
-  baseURL: 'https://etapas-practicas-ep.onrender.com',
-  headers: {
-    "token": token.token
-  }
+    // baseURL: 'http://localhost:4600/api',
+    baseURL: 'https://etapas-practicas-ep.onrender.com',
+    headers: {
+        "token": token 
+    }
 });
 
+apiClient.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token'); 
+    if (token) {
+        config.headers['token'] = token; 
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
 
-export default apiClient;
+export default apiClient;
